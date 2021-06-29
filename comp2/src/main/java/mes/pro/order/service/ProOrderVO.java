edc47d64e746a0dea36a7d146d13ba76c4344b6f
303120 <303120@naver.com>
2021-06-29 14:48:57 +0900

    210629 김다희 오전작업

--- comp2/src/main/java/mes/pro/order/service/ProOrderVO.java
+++ comp2/src/main/java/mes/pro/order/service/ProOrderVO.java
@@ -1,5 +1,7 @@
 package mes.pro.order.service;
 
+import lombok.Data;
+
 /**
  * @Class Name : ProOrderVO.java
  * @Description : ProOrder VO class
@@ -12,62 +14,15 @@ package mes.pro.order.service;
  *  
  *  Copyright (C)  All right reserved.
  */
+
+@Data
 public class ProOrderVO extends ProOrderDefaultVO{
     private static final long serialVersionUID = 1L;
-    
-    /** PRO_ORDER_CODE */
     private java.lang.String proOrderCode;
-    
-    /** PRO_ORDER_GUBUN */
     private java.lang.String proOrderGubun;
-    
-    /** PRO_ORDER_QTY */
     private java.math.BigDecimal proOrderQty;
-    
-    /** PRO_ORDER_RANK */
     private java.math.BigDecimal proOrderRank;
-    
-    /** PRO_ORDER_DATE */
     private java.sql.Date proOrderDate;
     
-    public java.lang.String getProOrderCode() {
-        return this.proOrderCode;
-    }
-    
-    public void setProOrderCode(java.lang.String proOrderCode) {
-        this.proOrderCode = proOrderCode;
-    }
-    
-    public java.lang.String getProOrderGubun() {
-        return this.proOrderGubun;
-    }
-    
-    public void setProOrderGubun(java.lang.String proOrderGubun) {
-        this.proOrderGubun = proOrderGubun;
-    }
-    
-    public java.math.BigDecimal getProOrderQty() {
-        return this.proOrderQty;
-    }
-    
-    public void setProOrderQty(java.math.BigDecimal proOrderQty) {
-        this.proOrderQty = proOrderQty;
-    }
-    
-    public java.math.BigDecimal getProOrderRank() {
-        return this.proOrderRank;
-    }
-    
-    public void setProOrderRank(java.math.BigDecimal proOrderRank) {
-        this.proOrderRank = proOrderRank;
-    }
-    
-    public java.sql.Date getProOrderDate() {
-        return this.proOrderDate;
-    }
-    
-    public void setProOrderDate(java.sql.Date proOrderDate) {
-        this.proOrderDate = proOrderDate;
-    }
-    
+   
 }
--- comp2/src/main/java/mes/pro/order/web/ProOrderController.java
+++ comp2/src/main/java/mes/pro/order/web/ProOrderController.java
@@ -1,6 +1,8 @@
 package mes.pro.order.web;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
@@ -10,6 +12,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
@@ -43,17 +46,34 @@ public class ProOrderController {
     /** EgovPropertyService */
     @Resource(name = "propertiesService")
     protected EgovPropertyService propertiesService;
-	
-    /**
-	 * PRO_ORDER 목록을 조회한다. (pageing)
-	 * @param searchVO - 조회할 정보가 담긴 ProOrderDefaultVO
-	 * @return "/proOrder/ProOrderList"
-	 * @exception Exception
-	 */
+    
+    //테이블 목록 조회
+    @RequestMapping("proOrder/ProdView")
+    @ResponseBody
+    public Map<String, Object> readOrder(Model model, @ModelAttribute("searchVO") ProOrderDefaultVO searchVO ) throws Exception {
+
+    	int rowSize = proOrderService.selectProOrderListTotCnt(searchVO);
+    	searchVO.setLastIndex(rowSize);
+    	List<?> list = proOrderService.selectProOrderList(searchVO);
+    	
+    	Map<String, Object> paging = new HashMap<>();
+    	paging.put("page", searchVO.getPageIndex());
+    	paging.put("totalCount", rowSize);
+    	
+    	Map<String, Object> data = new HashMap<>();
+    	data.put("contents", list);
+    	data.put("pagination", rowSize);
+    	
+    	Map<String, Object> map = new HashMap<>();
+    	map.put("result", true);
+    	map.put("data", data);    	
+    	
+    	return map;
+    }
+    
+    //생산지시 리스트 조회
     @RequestMapping(value="/proOrder/ProOrderList.do")
-    public String selectProOrderList(@ModelAttribute("searchVO") ProOrderDefaultVO searchVO, 
-    		ModelMap model)
-            throws Exception {
+    public String selectProOrderList(@ModelAttribute("searchVO") ProOrderDefaultVO searchVO, ModelMap model) throws Exception {
     	
     	/** EgovPropertyService.sample */
     	searchVO.setPageUnit(propertiesService.getInt("pageUnit"));
@@ -76,65 +96,65 @@ public class ProOrderController {
 		paginationInfo.setTotalRecordCount(totCnt);
         model.addAttribute("paginationInfo", paginationInfo);
         
-        return "mes/proOrder/ProOrderList.page";
+        return "mes/pro/order/prodView.page";
     } 
     
+    
+    //생산지시 폼 호출
     @RequestMapping("/proOrder/addProOrderView.do")
-    public String addProOrderView(
-            @ModelAttribute("searchVO") ProOrderDefaultVO searchVO, Model model)
-            throws Exception {
+    public String addProOrderView(@ModelAttribute("searchVO") ProOrderDefaultVO searchVO, Model model) throws Exception {
         model.addAttribute("proOrderVO", new ProOrderVO());
-        return "mes/proOrder/ProOrderRegister.page";
+        return "mes/pro/order/prodForm.page";
     }
     
-    @RequestMapping("/proOrder/addProOrder.do")
-    public String addProOrder(
-            ProOrderVO proOrderVO,
-            @ModelAttribute("searchVO") ProOrderDefaultVO searchVO, SessionStatus status)
-            throws Exception {
-        proOrderService.insertProOrder(proOrderVO);
-        status.setComplete();
-        return "forward:/proOrder/ProOrderList.do";
-    }
-    
-    @RequestMapping("/proOrder/updateProOrderView.do")
-    public String updateProOrderView(
-            @RequestParam("proOrderCode") java.lang.String proOrderCode ,
-            @ModelAttribute("searchVO") ProOrderDefaultVO searchVO, Model model)
-            throws Exception {
-        ProOrderVO proOrderVO = new ProOrderVO();
-        proOrderVO.setProOrderCode(proOrderCode);
-        // 변수명은 CoC 에 따라 proOrderVO
-        model.addAttribute(selectProOrder(proOrderVO, searchVO));
-        return "mes/proOrder/ProOrderRegister.page";
-    }
-
-    @RequestMapping("/proOrder/selectProOrder.do")
-    public @ModelAttribute("proOrderVO")
-    ProOrderVO selectProOrder(
-            ProOrderVO proOrderVO,
-            @ModelAttribute("searchVO") ProOrderDefaultVO searchVO) throws Exception {
-        return proOrderService.selectProOrder(proOrderVO);
-    }
-
-    @RequestMapping("/proOrder/updateProOrder.do")
-    public String updateProOrder(
-            ProOrderVO proOrderVO,
-            @ModelAttribute("searchVO") ProOrderDefaultVO searchVO, SessionStatus status)
-            throws Exception {
-        proOrderService.updateProOrder(proOrderVO);
-        status.setComplete();
-        return "forward:/proOrder/ProOrderList.do";
-    }
-    
-    @RequestMapping("/proOrder/deleteProOrder.do")
-    public String deleteProOrder(
-            ProOrderVO proOrderVO,
-            @ModelAttribute("searchVO") ProOrderDefaultVO searchVO, SessionStatus status)
-            throws Exception {
-        proOrderService.deleteProOrder(proOrderVO);
-        status.setComplete();
-        return "forward:/proOrder/ProOrderList.do";
-    }
+//    @RequestMapping("/proOrder/addProOrder.do")
+//    public String addProOrder(
+//            ProOrderVO proOrderVO,
+//            @ModelAttribute("searchVO") ProOrderDefaultVO searchVO, SessionStatus status)
+//            throws Exception {
+//        proOrderService.insertProOrder(proOrderVO);
+//        status.setComplete();
+//        return "forward:/proOrder/ProOrderList.do";
+//    }
+//    
+//    @RequestMapping("/proOrder/updateProOrderView.do")
+//    public String updateProOrderView(
+//            @RequestParam("proOrderCode") java.lang.String proOrderCode ,
+//            @ModelAttribute("searchVO") ProOrderDefaultVO searchVO, Model model)
+//            throws Exception {
+//        ProOrderVO proOrderVO = new ProOrderVO();
+//        proOrderVO.setProOrderCode(proOrderCode);
+//        // 변수명은 CoC 에 따라 proOrderVO
+//        model.addAttribute(selectProOrder(proOrderVO, searchVO));
+//        return "mes/pro/order/prodForm.page";
+//    }
+//
+//    @RequestMapping("/proOrder/selectProOrder.do")
+//    public @ModelAttribute("proOrderVO")
+//    ProOrderVO selectProOrder(
+//            ProOrderVO proOrderVO,
+//            @ModelAttribute("searchVO") ProOrderDefaultVO searchVO) throws Exception {
+//        return proOrderService.selectProOrder(proOrderVO);
+//    }
+//
+//    @RequestMapping("/proOrder/updateProOrder.do")
+//    public String updateProOrder(
+//            ProOrderVO proOrderVO,
+//            @ModelAttribute("searchVO") ProOrderDefaultVO searchVO, SessionStatus status)
+//            throws Exception {
+//        proOrderService.updateProOrder(proOrderVO);
+//        status.setComplete();
+//        return "forward:/proOrder/ProOrderList.do";
+//    }
+//    
+//    @RequestMapping("/proOrder/deleteProOrder.do")
+//    public String deleteProOrder(
+//            ProOrderVO proOrderVO,
+//            @ModelAttribute("searchVO") ProOrderDefaultVO searchVO, SessionStatus status)
+//            throws Exception {
+//        proOrderService.deleteProOrder(proOrderVO);
+//        status.setComplete();
+//        return "forward:/proOrder/ProOrderList.do";
+//    }
 
 }
