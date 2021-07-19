6c07c758937226c13a2d500373d4dc82551d4153
hanseol <gkstjf22@naver.com>
2021-07-19 12:37:31 +0900

    Update HomeController.java

--- comp2/src/main/java/mes/main/web/HomeController.java
+++ comp2/src/main/java/mes/main/web/HomeController.java
@@ -2,13 +2,26 @@ package mes.main.web;
 
 
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.annotation.Resource;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import egovframework.com.cmm.LoginVO;
+import egovframework.com.cmm.util.EgovUserDetailsHelper;
+import egovframework.com.sym.mnu.mpm.service.EgovMenuManageService;
+import egovframework.com.sym.mnu.mpm.service.MenuManageVO;
+import egovframework.com.utl.fcc.service.EgovStringUtil;
 import egovframework.rte.fdl.property.EgovPropertyService;
 import mes.pro.plan.service.ProPlanService;
 
@@ -28,13 +41,42 @@ public class HomeController {
 	@Resource(name = "propertiesService")
     protected EgovPropertyService propertiesService;
 	
+	//메뉴 목록 조회
+	/** EgovMenuManageService */
+	@Resource(name = "meunManageService")
+    private EgovMenuManageService menuManageService;
+	
 	/**
 	 * 홈 
 	 * 
 	 * */
+	@RequestMapping("/mes/getMenuLeft.do")
+	@ResponseBody
+	public Map<String,Object> getMenuLeft(ModelMap model,
+			@ModelAttribute("menuManageVO") MenuManageVO menuManageVO) throws Exception {
+		
+		LoginVO user =
+				(LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+
+	    	menuManageVO.setTmpId(user == null ? "" : EgovStringUtil.isNullToString(user.getId()));
+	    	menuManageVO.setTmpPassword(user == null ? "" : EgovStringUtil.isNullToString(user.getPassword()));
+	    	menuManageVO.setTmpUserSe(user == null ? "" : EgovStringUtil.isNullToString(user.getUserSe()));
+	    	menuManageVO.setTmpName(user == null ? "" : EgovStringUtil.isNullToString(user.getName()));
+	    	menuManageVO.setTmpEmail(user == null ? "" : EgovStringUtil.isNullToString(user.getEmail()));
+	    	menuManageVO.setTmpOrgnztId(user == null ? "" : EgovStringUtil.isNullToString(user.getOrgnztId()));
+	    	menuManageVO.setTmpUniqId(user == null ? "" : EgovStringUtil.isNullToString(user.getUniqId()));
+		
+		
+		List<?> list = menuManageService.selectMainMenuLeft(menuManageVO);
+		System.out.println("=============="+list.toString());
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("menuList",list);
+		
+		return map;
+	}
 	
 	@RequestMapping("/home.do")
-	public String home(HttpSession session) {
+	public String setContent(HttpServletRequest request, HttpServletResponse response, ModelMap model) throws Exception {
 
 		return "tiles/home.page";
 	}
@@ -50,7 +92,7 @@ public class HomeController {
     
     @RequestMapping("/accessDenied.do")
   	public String accessDenied(Model model) {
-  		return "mes/member/denied";
+  		return "mes/member/denied.page";
   	}
 
 }
